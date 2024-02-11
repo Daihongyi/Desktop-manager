eb86cd4ba6a6cbb4bcc8942b9016ca1b56a361e0
Daihongyi <159600819+Daihongyi@users.noreply.github.com>
2024-02-11 09:40:23 +0800

    move for Desktop Internet

--- Desktop Internet/Desktop Internet1.0(beta)/Desktop Internet1.0(beta).py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d4113e1877e7bba449d0e8e1174c2f0079531a90e04b7df6e94bae019db50427
-size 216
--- Desktop Internet/Desktop Internet1.0(beta)/readme.txt
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1497683676ab2ed01b7add33e06bce7f77a70fafb3167c47a5ea82f44b262362
-size 60
