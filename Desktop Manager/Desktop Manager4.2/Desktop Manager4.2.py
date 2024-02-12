5115b91b2602e8530bc2b50e99ff768fc95b7134
Daihongyi <159600819+Daihongyi@users.noreply.github.com>
2024-02-12 17:08:06 +0800

    move for install

--- Desktop Manager/Desktop Manager4.2/Desktop Manager4.2.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:13215535205664bf06deb459f3f44413604259bae3b30c9c3c2700fe5cba5a58
-size 4077
--- Desktop Manager/Desktop Manager4.3/Desktop Manager4.3.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1533b5e9d6566b591f27258ebbd03ec211f55e918a12ce0f294996676ec84c8e
-size 5416
